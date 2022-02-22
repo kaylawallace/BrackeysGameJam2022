01b2d2239a657b6677cd8a15d511cb7a0a6c7c12
Kayla Wallace <ktwallace511@gmail.com>
2022-02-22 18:23:25 +0000

    Added player dash

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -6,24 +6,49 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float dashSpeed = 10f;
+    public float startDashTime = 1f;
 
     private Rigidbody2D rb;
     private Vector2 movInput = Vector2.zero;
     private bool facingRight = true;
+    private bool dashed = false, canDash = true;
+    private float dashTime;
+    private float cooldownTime = 1f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
     }
 
     private void FixedUpdate()
     {
         Move();
+
+        if (dashed && canDash)
+        {
+            Dash();
+        }      
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movInput * speed * Time.fixedDeltaTime);
+    }
+
+    private void Dash()
+    {
+        if (dashTime <= 0)
+        {
+            dashTime = startDashTime;
+            StartCoroutine(Cooldown(cooldownTime));
+        }
+        else
+        {
+            rb.MovePosition(rb.position + movInput * dashSpeed * Time.fixedDeltaTime);
+            dashTime -= Time.deltaTime;
+        }
     }
 
     private void Flip()
@@ -34,5 +59,17 @@ public class PlayerMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         movInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        dashed = context.action.triggered;
+    }
+
+    IEnumerator Cooldown(float cooldownTime)
+    {        
+        canDash = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canDash = true;
     }
 }
