db337683792bb332245def2456d94efbeb508616
litsterCam <jack.litstercampbell@gmail.com>
2022-02-24 18:39:57 +0000

    Added player death/damage transitions

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int maxHealth;
-    public HealthBar healthBar;
+    private Animator anim;
+  //  public HealthBar healthBar;
 
     private int health;
     
     void Start()
     {  
         health = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        // healthBar.SetMaxHealth(maxHealth);
+        anim = GetComponentInChildren<Animator>();
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
-        healthBar.SetHealth(health);
+        // healthBar.SetHealth(health);
+        anim.SetTrigger("Damage");
 
         if (health <= 0)
         {
