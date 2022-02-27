6eec699669dbd5a3d227bbb6cf4eb112956682c5
litsterCam <jack.litstercampbell@gmail.com>
2022-02-27 09:36:28 +0000

    Added audio manager prefab

---
+++ Assets/Scripts/Audio_Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+       
+        s.source.Play();
+        
+    }
+
+    public void PlayLoop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (!s.source.isPlaying)
+        {
+        s.source.Play();
+        }
+    }
+
+    public void PlayDelayed(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(!s.source.isPlaying)
+        {
+        s.source.PlayDelayed(0.5f);
+        }
+    }
+
+     public void StopPlaying (string name)
+    {
+    Sound s = Array.Find(sounds, sound => sound.name == name);
+    s.source.Stop ();
+    }
+    
+}
